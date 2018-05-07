2a6ecf71c2a8397ec52b860553e836fc6f2f3876
Taylor Gratzer <taylor.gratzer@superion.com>
2018-05-07 14:00:37 -0400

    Add presidents with order, name, and term start/end year

---
+++ index.js
@@ -0,0 +1,361 @@
+exports.presidents = [
+  {
+    order: 1,
+    name: "George Washington",
+    term: {
+      startYear: 1789,
+      endYear: 1797
+    }
+  },
+  {
+    order: 2,
+    name: "John Adams",
+    term: {
+      startYear: 1797,
+      endYear: 1801
+    }
+  },
+  {
+    order: 3,
+    name: "Thomas Jefferson",
+    term: {
+      startYear: 1801,
+      endYear: 1809
+    }
+  },
+  {
+    order: 4,
+    name: "James Madison",
+    term: {
+      startYear: 1809,
+      endYear: 1817
+    }
+  },
+  {
+    order: 5,
+    name: "James Monroe",
+    term: {
+      startYear: 1817,
+      endYear: 1825
+    }
+  },
+  {
+    order: 6,
+    name: "John Quincy Adams",
+    term: {
+      startYear: 1825,
+      endYear: 1829
+    }
+  },
+  {
+    order: 7,
+    name: "Andrew Jackson",
+    term: {
+      startYear: 1829,
+      endYear: 1837
+    }
+  },
+  {
+    order: 8,
+    name: "Martin Van Buren",
+    term: {
+      startYear: 1837,
+      endYear: 1841
+    }
+  },
+  {
+    order: 9,
+    name: "William Henry Harrison",
+    term: {
+      startYear: 1841,
+      endYear: 1841
+    }
+  },
+  {
+    order: 10,
+    name: "John Tyler",
+    term: {
+      startYear: 1841,
+      endYear: 1845
+    }
+  },
+  {
+    order: 11,
+    name: "James K. Polk",
+    term: {
+      startYear: 1845,
+      endYear: 1849
+    }
+  },
+  {
+    order: 12,
+    name: "Zachary Taylor",
+    term: {
+      startYear: 1849,
+      endYear: 1850
+    }
+  },
+  {
+    order: 13,
+    name: "Millard Fillmore",
+    term: {
+      startYear: 1850,
+      endYear: 1853
+    }
+  },
+  {
+    order: 14,
+    name: "Franklin Pierce",
+    term: {
+      startYear: 1853,
+      endYear: 1857
+    }
+  },
+  {
+    order: 15,
+    name: "James Buchanan",
+    term: {
+      startYear: 1857,
+      endYear: 1861
+    }
+  },
+  {
+    order: 16,
+    name: "Abraham Lincoln",
+    term: {
+      startYear: 1861,
+      endYear: 1865
+    }
+  },
+  {
+    order: 17,
+    name: "Andrew Johnson",
+    term: {
+      startYear: 1865,
+      endYear: 1869
+    }
+  },
+  {
+    order: 18,
+    name: "Ulysses S. Grant",
+    term: {
+      startYear: 1869,
+      endYear: 1877
+    }
+  },
+  {
+    order: 19,
+    name: "Rutherford B. Hayes",
+    term: {
+      startYear: 1877,
+      endYear: 1881
+    }
+  },
+  {
+    order: 20,
+    name: "James A. Garfield",
+    term: {
+      startYear: 1881,
+      endYear: 1881
+    }
+  },
+  {
+    order: 21,
+    name: "Chester Arthur",
+    term: {
+      startYear: 1881,
+      endYear: 1885
+    }
+  },
+  {
+    order: 22,
+    name: "Grover Cleveland",
+    term: {
+      startYear: 1885,
+      endYear: 1889
+    }
+  },
+  {
+    order: 23,
+    name: "Benjamin Harrison",
+    term: {
+      startYear: 1889,
+      endYear: 1893
+    }
+  },
+  {
+    order: 24,
+    name: "Grover Cleveland",
+    term: {
+      startYear: 1893,
+      endYear: 1897
+    }
+  },
+  {
+    order: 25,
+    name: "William McKinley",
+    term: {
+      startYear: 1897,
+      endYear: 1901
+    }
+  },
+  {
+    order: 26,
+    name: "Theodore Roosevelt",
+    term: {
+      startYear: 1901,
+      endYear: 1909
+    }
+  },
+  {
+    order: 27,
+    name: "William Howard Taft",
+    term: {
+      startYear: 1909,
+      endYear: 1913
+    }
+  },
+  {
+    order: 28,
+    name: "Woodrow Wilson",
+    term: {
+      startYear: 1913,
+      endYear: 1921
+    }
+  },
+  {
+    order: 29,
+    name: "Warren G. Harding",
+    term: {
+      startYear: 1921,
+      endYear: 1923
+    }
+  },
+  {
+    order: 30,
+    name: "Calvin Coolidge",
+    term: {
+      startYear: 1923,
+      endYear: 1929
+    }
+  },
+  {
+    order: 31,
+    name: "Herbert Hoover",
+    term: {
+      startYear: 1929,
+      endYear: 1933
+    }
+  },
+  {
+    order: 32,
+    name: "Franklin D. Roosevelt",
+    term: {
+      startYear: 1933,
+      endYear: 1945
+    }
+  },
+  {
+    order: 33,
+    name: "Harry S. Truman",
+    term: {
+      startYear: 1945,
+      endYear: 1953
+    }
+  },
+  {
+    order: 34,
+    name: "Dwight D. Eisenhower",
+    term: {
+      startYear: 1953,
+      endYear: 1961
+    }
+  },
+  {
+    order: 35,
+    name: "John F. Kennedy",
+    term: {
+      startYear: 1961,
+      endYear: 1963
+    }
+  },
+  {
+    order: 36,
+    name: "Lyndon B. Johnson",
+    term: {
+      startYear: 1963,
+      endYear: 1969
+    }
+  },
+  {
+    order: 37,
+    name: "Richard Nixon",
+    term: {
+      startYear: 1969,
+      endYear: 1974
+    }
+  },
+  {
+    order: 38,
+    name: "Gerald Ford",
+    term: {
+      startYear: 1974,
+      endYear: 1977
+    }
+  },
+  {
+    order: 39,
+    name: "Jimmy Carter",
+    term: {
+      startYear: 1977,
+      endYear: 1981
+    }
+  },
+  {
+    order: 40,
+    name: "Ronald Reagan",
+    term: {
+      startYear: 1981,
+      endYear: 1989
+    }
+  },
+  {
+    order: 41,
+    name: "George Bush",
+    term: {
+      startYear: 1989,
+      endYear: 1993
+    }
+  },
+  {
+    order: 42,
+    name: "Bill Clinton",
+    term: {
+      startYear: 1993,
+      endYear: 2001
+    }
+  },
+  {
+    order: 43,
+    name: "George W. Bush",
+    term: {
+      startYear: 2001,
+      endYear: 2009
+    }
+  },
+  {
+    order: 44,
+    name: "Barack Obama",
+    term: {
+      startYear: 2009,
+      endYear: 2017
+    }
+  },
+  {
+    order: 45,
+    name: "Donald Trump",
+    term: {
+      startYear: 2017
+    }
+  },
+];
